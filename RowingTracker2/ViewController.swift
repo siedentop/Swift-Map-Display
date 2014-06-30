c2325643e11d4d1aa9a66282c287da0e9160d8e7
Christoph Siedentop <christoph@siedentop.name>
2014-06-30 21:10:19 +0200

    Initial Commit

---
+++ RowingTracker2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RowingTracker2
+//
+//  Created by Christoph Siedentop on 30.06.14.
+//  Copyright (c) 2014 Christoph Siedentop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RowingTracker2Tests/RowingTracker2Tests.swift
@@ -0,0 +1,35 @@
+//
+//  RowingTracker2Tests.swift
+//  RowingTracker2Tests
+//
+//  Created by Christoph Siedentop on 30.06.14.
+//  Copyright (c) 2014 Christoph Siedentop. All rights reserved.
+//
+
+import XCTest
+
+class RowingTracker2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
