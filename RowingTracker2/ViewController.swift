2191aabddbf83415be3d23464047107c8b62f4f9
Christoph Siedentop <christoph@siedentop.name>
2014-06-30 21:45:38 +0200

    Display a map
    User location is not shown because app doesn’t ask
    for permission.

--- RowingTracker2/ViewController.swift
+++ RowingTracker2/ViewController.swift
@@ -6,20 +6,25 @@
 //  Copyright (c) 2014 Christoph Siedentop. All rights reserved.
 //
 
-import UIKit
+    import UIKit
+    import MapKit
+    import CoreLocation
 
-class ViewController: UIViewController {
-                            
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    class ViewController: UIViewController, MKMapViewDelegate {
+        @IBOutlet var mapview: MKMapView = nil
+        var locationmgr : CLLocationManager!
+                                
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+            locationmgr = CLLocationManager()
+            locationmgr.requestWhenInUseAuthorization()
+            mapview.showsUserLocation = true
+        }
+
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
 
